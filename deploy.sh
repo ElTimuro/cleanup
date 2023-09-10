50bfbb92af95c7b436e4f3611be8fc4e9e5c118e
Timur Ercan <timur.ercan31@gmail.com>
2023-09-10 17:56:39 +0200

    chore: docs

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+# Run this to move the script to the right place
+# Be sure to the dir in PATH and give execution permissions using chmod +x
 echo -n "🚀 Deploying..."
 chmod +x cleanup
 sudo cp cleanup /usr/local/bin/cleanup
